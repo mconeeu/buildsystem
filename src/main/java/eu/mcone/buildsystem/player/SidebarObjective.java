749b0e85b75dd9e0fdffa47d23bb3a84b930f5c5
Dominik Lippl <dominik.lippl@hotmail.de>
2020-07-18 17:46:41 +0200

    Fixed Scoreboard.

--- src/main/java/eu/mcone/buildsystem/player/SidebarObjective.java
+++ src/main/java/eu/mcone/buildsystem/player/SidebarObjective.java
@@ -7,6 +7,7 @@ package eu.mcone.buildsystem.player;
 
 import eu.mcone.coresystem.api.bukkit.player.CorePlayer;
 import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjective;
+import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjectiveEntry;
 
 public class SidebarObjective extends CoreSidebarObjective {
 
@@ -15,22 +16,21 @@ public class SidebarObjective extends CoreSidebarObjective {
     }
 
     @Override
-    public void onRegister(CorePlayer player) {
-        setDisplayName("§7§l⚔ §e§l§nBuild Server");
+    protected void onRegister(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
+        entry.setTitle("§7§l⚔ §e§l§nBuild Server");
 
-        setScore(7, "");
-        setScore(6, "§7§oBenutze /plot help");
-        setScore(5, "§7§ofür Hilfe");
-        setScore(4, "");
-        setScore(3, "§8» §7Welt:");
-        onReload(player);
-        setScore(1, "");
-        setScore(0, "§f§lMCONE.EU");
+        entry.setScore(7, "");
+        entry.setScore(6, "§7§oBenutze /plot help");
+        entry.setScore(5, "§7§ofür Hilfe");
+        entry.setScore(4, "");
+        entry.setScore(3, "§8» §7Welt:");
+        onReload(player, entry);
+        entry.setScore(1, "");
+        entry.setScore(0, "§f§lMCONE.EU");
     }
 
     @Override
-    public void onReload(CorePlayer player) {
-        setScore(2, "§f§o" + player.getWorld().getName());
+    protected void onReload(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
+        entry.setScore(2, "§f§o" + player.getWorld().getName());
     }
-
 }
